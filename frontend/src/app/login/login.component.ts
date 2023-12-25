492115fe7c5b395076986d3ff148565a38825c35
Severin Selmaier <severin.selmaier@appsalot.de>
2023-12-25 09:14:49 +0100

    login with fake user and message

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass'
 })
 export class LoginComponent {
+
+  username = new FormControl('');
+  password = new FormControl('');
+
+  login() {
+    if (this.username.value == 'admin' && this.password.value == 'admin') {
+      alert('Login successful!');
+    } else {
+      alert('Login failed!');
+    }
+  }
 
 }
