5d3a94454c924aa08959e9167c32eb6e414d4f38
Severin Selmaier <severin.selmaier@appsalot.de>
2023-12-26 12:56:09 +0100

    initial backend

---
+++ backend/src/main/java/de/selmaier/taiko/TaikoApplication.java
@@ -0,0 +1,13 @@
+package de.selmaier.taiko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaikoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaikoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/selmaier/taiko/TaikoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.selmaier.taiko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaikoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
