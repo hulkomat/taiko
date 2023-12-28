bcf6cb3b3083aaf2415843db2d81bfe114ebf05d
Severin Selmaier <severin.selmaier@appsalot.de>
2023-12-28 10:50:50 +0100

    persistence and auditing config

---
+++ backend/src/main/java/de/selmaier/taiko/configurations/PersistenceConfig.java
@@ -0,0 +1,10 @@
+package de.selmaier.taiko.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "de.selmaier.taiko")
+public class PersistenceConfig {
+    
+}
