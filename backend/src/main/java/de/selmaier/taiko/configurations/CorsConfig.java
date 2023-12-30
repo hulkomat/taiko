90175fec7c7f5f58bc2a333e6b854c982bc7ae62
Severin Selmaier <severin.selmaier@appsalot.de>
2023-12-30 13:31:51 +0100

    add cors config

---
+++ backend/src/main/java/de/selmaier/taiko/configurations/CorsConfig.java
@@ -0,0 +1,17 @@
+package de.selmaier.taiko.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
