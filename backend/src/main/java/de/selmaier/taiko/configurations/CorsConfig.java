dde8efe5dc7cb22874fcf1956565368200cde8fa
Severin Selmaier <severin.selmaier@appsalot.de>
2023-12-30 13:39:26 +0100

    add missing enabling of webmvc

--- backend/src/main/java/de/selmaier/taiko/configurations/CorsConfig.java
+++ backend/src/main/java/de/selmaier/taiko/configurations/CorsConfig.java
@@ -1,9 +1,11 @@
 package de.selmaier.taiko.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig {
     
     public WebMvcConfigurer corsConfigurer() {
