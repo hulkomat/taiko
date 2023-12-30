94c83a266babc47767e42c40d431de8fa5fd27e9
Severin Selmaier <severin.selmaier@appsalot.de>
2023-12-30 13:31:43 +0100

    add security config

---
+++ backend/src/main/java/de/selmaier/taiko/configurations/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package de.selmaier.taiko.configurations;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(Customizer.withDefaults())
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/auth/login", "/actuator/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(withDefaults())
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+}
